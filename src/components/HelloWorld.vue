58c8d663b337b4343734b56039a63cfba3c54190
Tang yicong <49635772+TANGYICONG@users.noreply.github.com>
2023-04-01 19:40:38 +0800

    vue3 template created

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script setup lang='ts'>
+const inputVal = ref('test')
+</script>
+
+<template>
+<div class="main-container">
+  <a-input type="text" v-model:value="inputVal" />
+  <span>{{ inputVal }}</span>
+</div>
+</template>
+
+<style lang='less' scoped>
+.main-container {
+  display: flex;
+  flex-direction: column;
+  font-size: 32px;
+
+  &>input {
+    font-size: 32px;
+    text-align: center;
+  }
+
+  &>span {
+    margin-top: 20px;
+  }
+}
+</style>
